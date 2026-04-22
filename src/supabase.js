16446e21639f0a0d5dfad5f13f70e74898f5ef34
Mahaashri <asmahaashri16@gmail.com>
2026-04-22 11:20:33 +0530

    add env config

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nqurhckkyjzstvkmxryy.supabase.co';      // paste your URL here
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xdXJoY2treWp6c3R2a214cnl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODEwNjAsImV4cCI6MjA5MjM1NzA2MH0.HomDghz9_Xs5GraT5BGixLSNSrGLWVVReWdnk2IrLLs';         // paste your anon key here
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
